a640a8f4941786801ed0a25f2238f6819a334195
student <Muthoniyahya@gmail.com>
2021-03-07 18:53:11 +0300

    assignment1

---
+++ assignment.js
@@ -0,0 +1,13 @@
+function fruit() {
+    var fruits_list = ["strawberries","kiwi","guava","mangoes","tomatoes"];
+    fruits_list.forEach(fruits_list => {
+        console.log(fruits_list);
+
+    });
+    fruits_list[2];
+    console.log(fruits_list[2]);
+}
+fruit();
+
+
+
